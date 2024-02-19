c01a5da48b3d67f35401261ce768660c690e7302
nageshkumarbs007@gmail.com <nageshkumarbs007@gmail.com>
2024-02-19 14:27:36 +0530

    Creating rep

---
+++ StudentsAPI/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] names = {"Nagesh", "Naveen", "Nandeesh"};
+
+            return Ok(names);
+        }
+
+    }
+}
